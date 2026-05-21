a0f57b621419e3b43cf7af195b3b01a5fc4a5216
Raelson Santos <raelsons45@gmail.com>
2026-05-21 10:55:18 -0300

    Add password length validation to API endpoint

--- password_creation_path.py
+++ password_creation_path.py
@@ -1,14 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from generator import generator_password
+from utils import is_valid_password_length
 
 
 password_router = APIRouter(prefix="/create_password", tags=["create_password"])
 
 @password_router.post("/generate_password")
 async def create_password(password_length:int):
+        if not is_valid_password_length(password_length):
+                raise HTTPException(
+                        status_code=422,
+                        detail="A senha deve conter no mínimo 12 caracteres."
+                )
         created_password = generator_password(password_length)
         return {
                 "password": created_password,
                 "length": password_length
         }
+
+
 
